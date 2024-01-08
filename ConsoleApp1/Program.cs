cec4b111541895fb9f4f4f9ab4634ca005dc1216
pol-robledillo <pol.robledillo.7e7@itb.cat>
2024-01-08 13:46:41 +0100

    afegit comptador de lletres. Closes #4

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,1 +1,56 @@
-﻿
+﻿using System;
+namespace M03UF2
+{
+    public class Ex2
+    {
+        public static void Main()
+        {
+
+        }
+        public static bool ValidateOdd(int num)
+        {
+            return num % 2 == 0;
+        }
+        public static int Potencia(int num, int exp)
+        {
+            int result = 1;
+
+            for (int i = 0; i < exp; i++)
+            {
+                result = result * num;
+            }
+
+            return result;
+        }
+        public static int RandomNumber(int min, int max)
+        {
+            Random ran = new Random();
+
+            return ran.Next(min, max);
+        }
+        public static int CountVowels(string word)
+        {
+            int count = 0;
+            foreach (char c in word)
+            {
+                if ((c == 'a') || (c == 'e') || (c == 'i') || (c == 'o') || (c == 'u'))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+        public static int CountConsonats(string word)
+        {
+            int count = 0;
+            foreach (char c in word)
+            {
+                if ((c != 'a') || (c != 'e') || (c != 'i') || (c != 'o') || (c != 'u'))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
