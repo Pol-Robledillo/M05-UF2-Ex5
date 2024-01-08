67b5a7a8719f275416b3892baf9c53fbb9589a2b
pol-robledillo <pol.robledillo.7e7@itb.cat>
2024-01-08 13:37:35 +0100

    afegida func potencia. Closes #2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿using System;
+namespace M03UF2
+{
+    public class Ex2
+    {
+        public static void Main()
+        {
+
+        }
+        public static bool ValidateOdd(int num)
+        {
+            return num % 2 == 0;
+        }
+        public static int Potencia(int num, int exp)
+        {
+            int result = 1;
+
+            for (int i = 0; i < exp; i++)
+            {
+                result = result * num;
+            }
+
+            return result;
+        }
+    }
+}
