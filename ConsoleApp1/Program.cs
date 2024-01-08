a0c06a4d090b790e2a04733f0753cafa52319297
pol-robledillo <pol.robledillo.7e7@itb.cat>
2024-01-08 13:41:01 +0100

    afegida funcio numero aleatori. Closes #3

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,1 +1,32 @@
-﻿
+﻿using System;
+namespace M03UF2
+{
+    public class Ex2
+    {
+        public static void Main()
+        {
+
+        }
+        public static bool ValidateOdd(int num)
+        {
+            return num % 2 == 0;
+        }
+        public static int Potencia(int num, int exp)
+        {
+            int result = 1;
+
+            for (int i = 0; i < exp; i++)
+            {
+                result = result * num;
+            }
+
+            return result;
+        }
+        public static int RandomNumber(int min, int max)
+        {
+            Random ran = new Random();
+
+            return ran.Next(min, max);
+        }
+    }
+}
